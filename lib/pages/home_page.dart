605fe777896a11776293e1d420a71f460f1e0b44
PrinceNanaPoku <princenanapoku64@gmail.com>
2024-12-13 12:53:50 +0000

    added firebase

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,16 +16,30 @@ class _HomePageState extends State<HomePage> {
   TextEditingController textController = TextEditingController();
 
   //open dialog box to add note
-  void openDialogBox() {
+  void openDialogBox({String? docID}) {
     showDialog(
         context: context,
         builder: (context) => AlertDialog(
-              content: const TextField(),
+              content: TextField(
+                controller: textController,
+              ),
               actions: [
                 ElevatedButton(
-                  onPressed: () => firestoreService.addNote(
-                    textController.text,
-                  ),
+                  onPressed: () {
+                    if (docID == null) {
+                      firestoreService.addNote(
+                        textController.text,
+                      );
+                    } else {
+                      firestoreService.updateNotes(docID, textController.text);
+                    }
+
+                    //clear text controller
+                    textController.clear();
+
+                    //close box
+                    Navigator.pop(context);
+                  },
                   child: const Text('Add'),
                 ),
               ],
@@ -44,6 +58,54 @@ class _HomePageState extends State<HomePage> {
           Icons.add,
         ),
       ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getNotesStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              //check if snapshot has data
+
+              List notesList = snapshot.data!.docs;
+
+              //return data as list
+              return ListView.builder(
+                  itemCount: notesList.length,
+                  itemBuilder: (context, index) {
+                    //get the individual doc
+                    DocumentSnapshot document = notesList[index];
+                    String docID = document.id;
+
+                    //get each note from doc
+                    Map<String, dynamic> data =
+                        document.data() as Map<String, dynamic>;
+                    String noteText = data['note'];
+
+                    //display as a list tile
+                    return ListTile(
+                      title: Text(noteText),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          //update button
+                          IconButton(
+                            onPressed: () => openDialogBox(docID: docID),
+                            icon: const Icon(Icons.update),
+                          ),
+                          //delete button
+                          IconButton(
+                            onPressed: () =>
+                                firestoreService.deleteNotes(docID),
+                            icon: const Icon(Icons.delete),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            } else {
+              return const Center(
+                child: Text('No Notes'),
+              );
+            }
+          }),
     );
   }
 }
--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -14,8 +14,21 @@ class FirestoreService {
   }
 
   //read: get note from db
+  Stream<QuerySnapshot> getNotesStream() {
+    final noteStream = notes.orderBy('timestamp', descending: true).snapshots();
+    return noteStream;
+  }
 
   //update: update notes given a doc id
+  Future<void> updateNotes(String docID, String newNote) {
+    return notes.doc(docID).update({
+      'note': newNote,
+      'timestamp': Timestamp.now(),
+    });
+  }
 
   //delete: delete notes given a doc id
+  Future<void> deleteNotes(String docID) {
+    return notes.doc(docID).delete();
+  }
 }
